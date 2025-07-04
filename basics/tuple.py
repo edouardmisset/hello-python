2141e6c77acb7ca9bcb671d1ffd9ab098ada67cf
Edouard Misset <edouardmisset@gmail.com>
2025-07-04 11:34:15 +0200

    Add tuple.py to demonstrate tuple creation and immutability

---
+++ basics/tuple.py
@@ -0,0 +1,11 @@
+"""Tuple"""
+
+# Immutable lists
+
+my_tuple = (1, 2, 3, 4)
+
+# my_tuple[1] = 0  # TypeError 'my_tuple' does not support item assignment
+
+print(4 in my_tuple)  # True
+
+# Less flexible but more performant
